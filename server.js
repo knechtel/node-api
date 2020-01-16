86f61bd862661e79fd309290b82f8ae97e3ce539
Maiquel Knechtel <maiquelknechtel@gmail.com>
2020-01-16 20:20:17 -0300

    test

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-
-
+var bodyParser = require('body-parser');
 app.use(cors())
-
+app.use(bodyParser.json());
 app.use('/',require('./config/routes'))
 
 app.listen(3001);
